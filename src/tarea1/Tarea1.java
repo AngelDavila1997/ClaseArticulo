2412f9afb26ac7a724226ea229ac55e3dfd07814
PC <PC@192.168.1.69>
2017-01-27 11:21:13 -0600

    Prueba en el main

--- src/tarea1/Tarea1.java
+++ src/tarea1/Tarea1.java
@@ -15,35 +15,20 @@ public class Tarea1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Declaracion de objetos
-        Articulo miArticulo = new Articulo(); 
-        Articulo miOtroArticulo = new Articulo(2, 3, 25.00, 10.00, "Articulo1");
-        //Muestra la informacion del objeto "miArticulo"
-        prueba(miArticulo);
-        //Muestra la informacion del objeto "miOtroArticulo" que contiene valores predeterminados
-        prueba(miOtroArticulo);
-        
-        //Demostracion de metodos de la clase
-        //Se comprara 1 articulo y se cambiara el costo a 24
-        miOtroArticulo.compra(1, 14.00);
-        //Se verifica la existencia despues de la compra (habia 3, ahora 4)
-        miOtroArticulo.verificarExistencia();
-        //Muestra el nuevo costo
-        System.out.println("nuevo costo = " + miOtroArticulo.getCosto());
-        //Se vendera 1 articulo
-        miOtroArticulo.venta(1);
-        //Se verifica la existencia despues de la compra (habia 4, ahora 3)
-        miOtroArticulo.verificarExistencia();
-        //Se venderan 12 articulos (como no hay suficientes se muestra un mensaje)
-        miOtroArticulo.venta(12);
-        //Se actualiza el precio del articulo (Antes era 25)
-        miOtroArticulo.actualizaPrecio(18.00);
-    }
-    public static void prueba (Articulo a){
-        System.out.println("sku = " + a.getSku());
-        System.out.println("existencia = " + a.getExistencia());
-        System.out.println("precio = " + a.getPrecio());
-        System.out.println("costo = " + a.getCosto());
-        System.out.println("descripcion = " + a.getDescripcion());
+        //Instancia de la clase Artículo
+        Articulo telefono = new Articulo(120899, 0,  11500.00, 0.00, "Iphone 6s 16Gb");
+        //Compra de 50 teléfonos
+        telefono.compra(50, 9890.00);
+        //Muestra la existencia en almacén
+        System.out.println("La existencia en almacén del artículo es " + 
+                            telefono.verificarExistencia());
+        //Se realiza una venta
+        System.out.println("Se venden 15 teléfonos");
+        telefono.venta(15);
+        System.out.println("La nueva existencia después de la venta es "+
+                            telefono.verificarExistencia());
+        //Se realiza actualización de precios
+        System.out.println("Hay una promo, y el nuevo precio es $10,000.00");
+        telefono.actualizaPrecio(10000.00);
     }
 }
